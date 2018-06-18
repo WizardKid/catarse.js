fa49fc84af82320b27980359487e2d85228d2eb0
Vinicius Andrade <vini.andrade.dev@gmail.com>
2018-06-18 22:42:37 +0200

    Add elm support in application

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,17 @@ module.exports = {
         filename: 'catarse.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.elm$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                loader: 'elm-webpack-loader',
+                options: {
+                    debug: true,
+                    warn: true
+                }
+            }
+        ]
+    }
 };
